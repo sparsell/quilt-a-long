6c63ba6aaaa26514cc0581bc41281076fe451ce1
sparsell <hello@sparsell.com>
2021-02-03 14:32:24 -0500

    started create user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
     end
 
     def create
+        if params[:user][:password] == params[:user][:password_confirmation]
+            @user = User.create(user_params)
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else
+            redirect_to '/'
+        end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
