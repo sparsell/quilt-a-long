92ace7b74517c1478fd593df2df3edb789fd4367
sparsell <hello@sparsell.com>
2021-02-04 09:59:16 -0500

    added helpers to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,10 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  get 'sign_up', to: 'users#new'
-  post '/sign_up', to: 'users#create'
-  get 'log_in', to: 'sessions#new'
-  post 'log_in', to: 'sessions#create'
+  get 'signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 
   resources :users
 end
