b78cdea3bf66feca1560608f409f528ce25162ac
sparsell <hello@sparsell.com>
2021-02-05 10:33:23 -0500

    added qal controler and view

--- app/controllers/qals_controller.rb
+++ app/controllers/qals_controller.rb
@@ -1,4 +1,28 @@
 class QalsController < ApplicationController
     # before_action :require_login
     
+    def new
+        @qal = Qal.new
+    end
+
+    def create
+        
+        @qal = Qal.create(qal_params)
+        if @qal 
+            redirect_to qal_path(@qal)
+        else
+            redirect_to new_qal_path
+        end
+    end
+
+    def show
+        @qal = Qal.find_by(id: params[:id])
+    end
+
+    private
+
+    def qal_params
+        params.require(:qal).permit(:qal_name, :start_date, :end_date)
+    end
+
 end
