d286947da5b406df68a7628bb6ca63d4a1c853d5
sparsell <hello@sparsell.com>
2021-02-13 15:32:54 -0500

    added sponsor associations to models; added to main nav bar

--- app/models/qal.rb
+++ app/models/qal.rb
@@ -1,6 +1,7 @@
 class Qal < ApplicationRecord
 
     belongs_to :user 
-    has_many :quilters 
+    has_many :quilters
+    has_many :sponsors 
     has_many :quilts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
 
     has_many :qals
     has_many :quilters, through: :qals
+    has_many :sponsors, through: :qals
 
 end
