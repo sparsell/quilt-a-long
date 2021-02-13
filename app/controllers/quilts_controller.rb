4237303b0186cbad3e6a7bf7252ed23604f6e654
sparsell <hello@sparsell.com>
2021-02-13 15:34:37 -0500

    added quilt views and controller actions

--- app/controllers/quilts_controller.rb
+++ app/controllers/quilts_controller.rb
@@ -1,2 +1,26 @@
 class QuiltsController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @quilt = Quilt.new
+        @quilters = Quilter.all
+    end
+
+    def create
+            @quilt = Quilt.create(quilt_params)
+            redirect_to quilt_path(@quilt)
+    end
+
+    def show
+        @quilt = Quilt.find_by(id: params[:id])
+    end
+
+    private
+
+        def quilt_params
+            params.require(:quilt).permit(:quilt_name) 
+        end
+
 end
