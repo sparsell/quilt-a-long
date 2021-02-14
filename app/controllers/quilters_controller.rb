fd53c33532d5e867140a23741ba9695a52e83a1a
sparsell <hello@sparsell.com>
2021-02-14 17:35:23 -0500

    labeling new quilter form

--- app/controllers/quilters_controller.rb
+++ app/controllers/quilters_controller.rb
@@ -2,9 +2,11 @@ class QuiltersController < ApplicationController
 
     def new
         @quilter = Quilter.new
+        @quilters = Quilter.all
     end
 
     def create
+        # binding.pry
         @quilter = current_user.quilters.build(quilter_params)
         if @quilter.save 
             redirect_to current_user
@@ -19,8 +21,20 @@ class QuiltersController < ApplicationController
     end
 
     def index
-        @quilters = Quilter.all
+        @quilters = Quilter.all 
     end
+
+    def quilts_index
+        @quilter = Quilter.find(params[:id])
+        @quilts = @quilter.quilts
+        render template: 'quilts/index'
+      end
+     
+      def quilt
+        @quilter = Quilter.find(params[:id])
+        @quilt = Quilt.find(params[:quilt_id])
+        render template: 'quilts/show'
+      end
 
     private
 
