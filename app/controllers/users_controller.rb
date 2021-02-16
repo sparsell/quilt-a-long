b22683bdad42413a798161bc9a4135b6f2ae56ed
sparsell <hello@sparsell.com>
2021-02-16 16:00:45 -0500

    added before_action to user edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
-    
+    before_action :logged_in_user, only: [:edit, :update]
 
     def new
         @user = User.new
     end
 
     def create
+        # binding.pry
         if params[:user][:password] == params[:user][:password_confirmation]
             @user = User.create(user_params)
             session[:user_id] = @user.id
-            redirect_to user_path
+            redirect_to login_path
         else
             flash[:message] = "That is not a valid signup. Please try again"
             render :new
@@ -43,6 +44,13 @@ class UsersController < ApplicationController
 
         def user_params
             params.require(:user).permit(:name, :email, :password, :password_confirmation, :company_name, :quilting_style)
+        end
+
+        def logged_in_user 
+            unless logged_in?
+                flash[:message] = "Please log in."
+                redirect_to login_url
+            end
         end
 
 end
