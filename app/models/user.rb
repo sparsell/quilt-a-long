74a7c9ce471ca85fb9ed6e8f71c0bae49c5154c8
sparsell <hello@sparsell.com>
2021-02-26 10:38:42 -0500

    wip nested attributes and index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
-    has_many :qals
+    has_many :qals, dependent: :destroy
     has_many :quilters, through: :qals
 
+    accepts_nested_attributes_for :qals
 end
