442eae6514af5e0ec223071bb25a20da418c1b2b
sparsell <hello@sparsell.com>
2021-03-02 14:28:27 -0500

    holding off on quilts for now

--- app/models/quilt.rb
+++ app/models/quilt.rb
@@ -1,23 +1,23 @@
 class Quilt < ApplicationRecord
 
-    belongs_to :qal
-    belongs_to :quilter
+    # belongs_to :qal
+    # belongs_to :quilter
    
-    def qal_name
-        self.qal ? self.qal.qal_name : nil
-     end
+    # def qal_name
+    #     self.qal ? self.qal.qal_name : nil
+    #  end
 
-    def qal_name=(name)
-        self.qal = Qal.find_or_create_by(qal_name: qal_name)
-      end
+    # def qal_name=(name)
+    #     self.qal = Qal.find_or_create_by(qal_name: qal_name)
+    #   end
 
-      def quilter_name  
-        self.quilter ? self.quilter.quilter_name : nil
-     end
+    #   def quilter_name  
+    #     self.quilter ? self.quilter.quilter_name : nil
+    #  end
 
-      def quilter_name=(name)
-        self.quilter = Quilter.find_or_create_by(quilter_name: quilter_name)
-      end
+    #   def quilter_name=(name)
+    #     self.quilter = Quilter.find_or_create_by(quilter_name: quilter_name)
+    #   end
     
      
 
--- app/models/quilter.rb
+++ app/models/quilter.rb
@@ -1,7 +1,7 @@
 class Quilter < ApplicationRecord
 
     belongs_to :qal
-    has_many :quilts 
+    # has_many :quilts 
    
-    accepts_nested_attributes_for :quilts
+    # accepts_nested_attributes_for :quilts
 end
