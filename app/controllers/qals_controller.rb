7635f1d910f760a6f65f46d5494b81017aa9231b
sparsell <hello@sparsell.com>
2021-03-03 14:15:20 -0500

    debugging check box for quilter selection

--- app/controllers/qals_controller.rb
+++ app/controllers/qals_controller.rb
@@ -52,7 +52,8 @@ class QalsController < ApplicationController
     private
 
     def qal_params
-        params.require(:qal).permit(:qal_name, :start_date, :end_date, :quilting_style, :skill_level, quilters_attributes: [:id, :quilter_name], prizes_attributes: [:id, :prize_name])
+        # params.require(:qal).permit(:qal_name, :start_date, :end_date, :quilting_style, :skill_level, quilters_attributes: [:id, :quilter_name])
+        params.require(:qal).permit(:qal_name, :start_date, :end_date, :quilting_style, :skill_level, quilter_ids:[])
     end
 
 end
