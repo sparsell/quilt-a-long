c739a76aa7881fb3231688bd63975ba773ca21cc
Sonja Parsell <hello@sparsell.com>
2021-03-29 16:46:06 -0400

    adding validations and fixed user association

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,8 +1,11 @@
 class Sponsor < ApplicationRecord
 
-    has_many :prizes
+    has_many :prizes, dependent: :destroy
     has_many :qals, through: :prizes
 
     accepts_nested_attributes_for :prizes
     accepts_nested_attributes_for :qals
+
+    validates :sponsor_name, uniqueness: :true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,11 @@ class User < ApplicationRecord
 
     has_many :qals, dependent: :destroy
     has_many :quilters, through: :qals
+    has_many :sponsors, through: :qals
 
     accepts_nested_attributes_for :qals
+    accepts_nested_attributes_for :quilters
+    accepts_nested_attributes_for :sponsors
+
       
 end
