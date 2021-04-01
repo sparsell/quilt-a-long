301666a97fa75f2ccfe7dbff96606644f23cbde1
Sonja Parsell <hello@sparsell.com>
2021-04-01 14:41:44 -0400

    added partial for new and edit; flash essages for create and destroy

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -1,33 +1,49 @@
 class PrizesController < ApplicationController
-
+      before_action :require_login
+      
     def index 
-        @prizes = Prize.all
+        if
+        @prizes = Prize.where(qal_id: params[:qal_id])
+        else 
+            flash[:message] = "There aren't any prizes for this Quilt-A-Long"
+        end
     end
 
     def show
-    @prize = Prize.find_by(id: params[:id])
+        @prize = Prize.find_by(id: params[:id])
     end
 
     def new
         @prize = Prize.new
-        # @sponsor = Sponsor.find_by(id: params[:sponsor_id])
-        # binding.pry
-       
     end
 
     def create
         @prize = Prize.create(prize_params)
+        @qal = params[:qal_id]
         if @prize.save
-            redirect_to prizes_path
+            flash[:message] = "Successfully added Prize."
+            redirect_to sponsors_path
+            # redirect_to qal_prizes_path(@qal)
         else
             render :new
         end
     end
 
     def edit
+        @prize = Prize.find_by(id: params[:id])
     end
 
     def update
+    end
+
+    def destroy
+        # binding.pry
+        @prize = Prize.find_by(id: params[:id])
+        if 
+        @prize.destroy
+            flash[:message] = "Prize was successfully destroyed."
+            redirect_to qals_path
+        end
     end
 
 end
